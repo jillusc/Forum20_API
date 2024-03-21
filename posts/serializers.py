8b9e9fe354d8da8b6693ed619ba80b9db2ae550d
jillusc <jillus@hotmail.com>
2024-03-21 14:23:39 +0000

    Wrote PostDetail view to handle CRUD operations for a single post

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from posts.models import Post
 
-
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
@@ -9,26 +8,24 @@ class PostSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def validate_image(self, value):
-        if value.size > 2 * 1024 * 1024:
-            raise serializers.ValidationError('Image size larger than 2MB!')
-        if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height larger than 4096px!'
-            )
-        if value.image.width > 4096:
-            raise serializers.ValidationError(
-                'Image width larger than 4096px!'
-            )
+        if hasattr(value, 'size'):
+            if value.size > 2 * 1024 * 1024:
+                raise serializers.ValidationError('Image size larger than 2MB!')
+            if value.image.height > 4096:
+                raise serializers.ValidationError('Image height larger than 4096px!')
+            if value.image.width > 4096:
+                raise serializers.ValidationError('Image width larger than 4096px!')
+        else:
+            return value
         return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
-    class Meta:
+    class Meta: 
         model = Post
         fields = [
-            'id', 'owner', 'is_owner', 'profile_id',
-            'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter'
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image', 
+            'created_at', 'updated_at', 'title', 'content', 'image', 'image_filter'
         ]
