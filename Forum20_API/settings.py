6f5d8a15b00f3bdc8d9efd88c1ba1ba663aa8470
jillusc <jillus@hotmail.com>
2024-03-22 13:45:50 +0000

    Set default renderer to JSON for production environment.

--- Forum20_API/settings.py
+++ Forum20_API/settings.py
@@ -37,6 +37,10 @@ REST_FRAMEWORK = {
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
