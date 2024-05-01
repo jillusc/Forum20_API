a167c8200c95d29417ebafa6cd3c30406f06cae8
jillusc <jillus@hotmail.com>
2024-05-01 12:25:38 +0000

    Modified Post serializer to include bookmarks functionality.

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from posts.models import Post
 from likes.models import Like
+from bookmarks.models import Bookmark
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -13,6 +14,7 @@ class PostSerializer(serializers.ModelSerializer):
     like_id = serializers.SerializerMethodField()
     likes_count = serializers.ReadOnlyField()
     comments_count = serializers.ReadOnlyField()
+    bookmark_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if not value:
@@ -44,10 +46,18 @@ class PostSerializer(serializers.ModelSerializer):
             return like.id if like else None
         return None
 
+    def get_bookmark_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            bookmark = Bookmark.objects.filter(owner=user, post=obj).first()
+            return bookmark.id if bookmark else None
+        return None
+
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'created_at', 'updated_at', 'title', 'content', 'image',
             'like_id', 'likes_count', 'comments_count', 'is_private',
+            'bookmark_id',
         ]
