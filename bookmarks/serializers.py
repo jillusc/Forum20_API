6528c803e7dbf42fd0dd20e81e367e62d6e2770e
jillusc <jillus@hotmail.com>
2024-05-01 16:52:47 +0000

    Added post_title field to bookmark serializer.

--- bookmarks/serializers.py
+++ bookmarks/serializers.py
@@ -5,10 +5,11 @@ from .models import Bookmark
 
 class BookmarkSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    post_title = serializers.ReadOnlyField(source='post.title')
 
     class Meta:
         model = Bookmark
-        fields = ['id', 'created_at', 'owner', 'post']
+        fields = ['id', 'created_at', 'owner', 'post', 'post_title']
 
     def create(self, validated_data):
         try:
