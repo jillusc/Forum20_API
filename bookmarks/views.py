7d685a9b6e6edd52fb24d19194062a77a14c8db7
jillusc <jillus@hotmail.com>
2024-05-03 08:50:07 +0000

    Added current user filter to BookmarkList view.

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -12,6 +12,9 @@ class BookmarkList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    def get_queryset(self):
+        return Bookmark.objects.filter(owner=self.request.user)
+
 class BookmarkDetail(generics.RetrieveDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = BookmarkSerializer
