2f0fb8805f7f77b37ee99456f7df640176da7cab
jillusc <jillus@hotmail.com>
2024-05-14 14:19:35 +0000

    Included post_title in the Likes serializer for displaying in Activity page.

--- likes/serializers.py
+++ likes/serializers.py
@@ -5,10 +5,11 @@ from .models import Like
 
 class LikeSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    post_title = serializers.ReadOnlyField(source='post.title')
 
     class Meta:
         model = Like
-        fields = ['id', 'created_at', 'owner', 'post']
+        fields = ['id', 'created_at', 'owner', 'post', 'post_title']
 
     def create(self, validated_data):
         try:
