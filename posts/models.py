2baf569e41d27f7dc7174413e320cb57dc3ebc3e
jillusc <jillus@hotmail.com>
2025-11-05 17:50:18 +0100

    Fix: squash images to improve performance

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
 
 class Post(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -14,6 +16,24 @@ class Post(models.Model):
     )
     artist_name = models.CharField(max_length=255, blank=True, null=True)
     year_of_artwork = models.IntegerField(blank=True, null=True)
+
+def save(self, *args, **kwargs):
+    # Call the parent save first if the object is new (so image has a path)
+    if not self.pk:
+        super().save(*args, **kwargs)
+
+    if self.image:
+        try:
+            img = Image.open(self.image)
+            img = img.convert('RGB')
+            buffer = BytesIO()
+            img.save(buffer, format='WEBP', quality=80)
+            # Replace the file with the compressed version
+            self.image.save(self.image.name, ContentFile(buffer.getvalue()), save=False)
+        except Exception as e:
+            print("Image compression failed:", e)
+
+    super().save(*args, **kwargs)
 
     class Meta:
         ordering = ['-created_at']
